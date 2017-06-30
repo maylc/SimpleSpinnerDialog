626a2a5c9b58e2b8651f38a9d53e66ec17e7272b
Mayara Felix <mayaralcabral@gmail.com>
2017-06-30 07:31:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/mayca/simplesearchablespinnerdialog/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.mayca.simplesearchablespinnerdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.mayca.simplesearchablespinnerdialog.model.User;
+import com.example.mayca.simplesearchablespinnerdialog.spinner.OnSpinnerItemClick;
+import com.example.mayca.simplesearchablespinnerdialog.spinner.SimpleSpinnerDialog;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<User> users = new ArrayList<User>();
+    SimpleSpinnerDialog gsd;
+    TextView userName;
+    TextView userId;
+    TextView userEmail;
+    TextView userPos;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /***************************************************************************************/
+
+        userName  = (TextView)findViewById(R.id.name);
+        userId    = (TextView)findViewById(R.id.id);
+        userEmail = (TextView)findViewById(R.id.email);
+        userPos   = (TextView)findViewById(R.id.position);
+        button    = (Button) findViewById(R.id.button);
+
+        /***************************************************************************************/
+        /** Example **/
+
+        populateUserArrayList();
+
+        gsd = new SimpleSpinnerDialog(MainActivity.this,users,"Simple Spinner Dialog Example");
+
+        gsd.bindOnSpinerListener(new OnSpinnerItemClick()
+        {
+            @Override
+            public void onClick(Object item, int position)
+            {
+                User selected = (User) item;
+
+                userPos.setText("Position: " + position);
+                userName.setText("Name: " + selected.getName());
+                userId.setText("ID: " + String.valueOf(selected.getId()));
+                userEmail.setText("Email: " + selected.getEmail());
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                gsd.show();
+            }
+        });
+    }
+
+    private void populateUserArrayList()
+    {
+        User user1 = new User(2, "Bob", "bob@mail.com");
+        User user2 = new User(7, "Ami", "ami@mail.com");
+        User user3 = new User(1, "John", "john@mail.com");
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+    }
+}
---
+++ app/src/main/java/com/example/mayca/simplesearchablespinnerdialog/spinner/OnSpinnerItemClick.java
@@ -0,0 +1,6 @@
+package com.example.mayca.simplesearchablespinnerdialog.spinner;
+
+public interface OnSpinnerItemClick
+{
+    void onClick(Object var1, int var2);
+}
